5bffe14133ebcd1756e2fed1770e9c4c89894645
dam2a <dhermidaperez@danielcastelao.org>
2023-03-08 14:00:07 +0100

    Cosas cambiadas

---
+++ RepasoGtk.py
@@ -0,0 +1,59 @@
+import gi
+from gi.overrides.Gtk import Gtk
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import *
+
+from conexionBD import conexionBD
+
+
+class treeview(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Exemplo TreeView CellRendererCombo")
+
+        caixaV = Gtk.Box(orientation=Gtk.orientation.VERTICAL, spacing=4)
+        modelo = Gtk.ListStore(str, str, int)
+        trvPerfilesUsuarios = Gtk.TreeView(model=modelo)
+        modeloPerfis = Gtk.ListStore(str, str)
+
+        celda = Gtk.CellRendererText()
+        columna = Gtk.TreeViewColumn("DNI", celda, text=0)
+        trvPerfilesUsuarios.append_column(columna)
+        celda2 = Gtk.CellRendererText()
+        columna2 = Gtk.TreeViewColumn("Nome", celda, text=1)
+        trvPerfilesUsuarios.append_column(columna2)
+
+        celda3 = Gtk.CellRendererText()
+        columna3 = Gtk.TreeViewColumn("perfil", celda, text=2)
+        trvPerfilesUsuarios.append_column(columna3)
+
+        bd = conexionBD("perfisUsuarios.bd")
+        conexBD = bd.conectaBD()
+        cursorBD = bd.creaCursor()
+        sqlUsuarios = "SELECT dni,nome  FROM usuario"
+        sqlPerfisUsuario = "SELECT idPerfilFROM perfisUsuario WHERE dni=?"
+        sqlPerfis = "SELECT descricion FROM perfis WHERE idperfil=?"
+        lUsuarios = bd.consultaSenParametros(sqlUsuarios)
+        usuariosPerfil = list()
+        for usuario in lUsuarios:
+            idPerfil = bd.consultaConParametros(sqlPerfisUsuario, usuario[0])
+            descripcionPerfil = bd.consultaConParametros(sqlPerfis, idPerfil[0])
+            elemento = list(usuario)
+            elemento.append(idPerfil[0][0])
+            elemento.append(descripcionPerfil[0][0])
+            print(elemento)
+            modelo.append(elemento)
+
+        def on_celda3_changed(self, crCombo, fila, elemento, modeloTrv, modeloCmbPerfis):
+            modeloTrv[fila][2] = modeloCmbPerfis[elemento][1]
+            modeloTrv[fila][3] = modeloCmbPerfis[elemento][0]
+        caixaV.pacl_start(trvPerfilesUsuarios, True, True, 0)
+
+        self.add(caixaV)
+        self.connect("destroy", Gtk.main_quit)
+        self.show_all()
+
+
+if __name__ == '__main__':
+    treeview()
+    Gtk.main()
